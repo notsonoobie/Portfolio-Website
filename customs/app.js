6e90b6927f021d2b6e14a4e1d110a7f6df732485
Rahul Gupta <swastikmedical74@gmail.com>
2019-09-27 19:39:18 +0530

    Added Smooth Scrolling, &  Navigation to the page

--- customs/app.js
+++ customs/app.js
@@ -1,13 +1,36 @@
 $(document).ready(function(){
+    // Adding a disclosure to the top of the page
     $("body").prepend(
         `<div class="disclosure"> NOTE : This Website is Compatible to be viewed with Large-Screen Devices (Mobile View is under Construction)<span class="right" title="Ignore">&#9747;</span></div>`
     );
     
+    // Toggling the navigation button (Basically unchechecking the checkbox with the help of label)
+    $(".navigation__link").click(function(e){
+        $(".navigation__button").trigger("click");
+    });
+
+    // Smooth Scrolling
+    $("a").click(function(e){
+
+        if(this.hash !== ""){
+            e.preventDefault();
+            let hash = this.hash;
+
+            $("html,body").animate({
+                scrollTop: $(hash).offset().top
+            },1000,function(){
+                window.location.hash = hash;
+            });
+        }
+
+    });
+    // Removing the Disclosure when Ignore button is clicked
     $(".right").click(function(e){
         e.preventDefault();
         $(".disclosure").remove();
     });
 
+    // API Call
     $.ajax({url: "http://quotes.rest/qod.json" , 
             type: "GET",
             success: function(response){
